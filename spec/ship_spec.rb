1a431536de7b905d0587c5908a736ee89d3c1d20
Derek Chavez <frrenose17@yahoo.com>
2023-05-25 19:25:59 -0600

    test: add exists test

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Ship do
     @cruiser = Ship.new("Cruiser", 3)
     @submarine = Ship.new("Submarine", 2)
   end
-
+  
   it 'exists' do
     expect(@cruiser).to be_an_instance_of(Ship)
     expect(@submarine).to be_an_instance_of(Ship)
