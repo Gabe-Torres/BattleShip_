dfba53b8fc9d4b5a31c0200ee6da68a566eb7a0d
Derek Chavez <frrenose17@yahoo.com>
2023-05-26 14:32:57 -0600

    test: write fired upon test

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -26,4 +26,9 @@ RSpec.describe Cell do
     expect(@cell.ship).to eq(@cruiser)
     expect(@cell.empty?).to eq(false)
   end
+
+  it 'can be fired upon' do
+    @cell.place_ship(@cruiser)
+    expect(@cell.fired_upon?).to eq(false)
+  end
 end
