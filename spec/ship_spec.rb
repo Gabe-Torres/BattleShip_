248b0d666accefc32ee210f299a4fb98a116c675
Derek Chavez <frrenose17@yahoo.com>
2023-05-25 19:24:27 -0600

    Initial commit

---
+++ spec/ship_spec.rb
@@ -0,0 +1,13 @@
+require './lib/ship'
+
+RSpec.describe Ship do
+  before(:each) do
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
+  end
+
+  it 'exists' do
+    expect(@cruiser).to be_an_instance_of(Ship)
+    expect(@submarine).to be_an_instance_of(Ship)
+  end
+end
