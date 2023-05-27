8e485eae0b96ef67613079f2bca93d7bfb502e4a
Derek Chavez <frrenose17@yahoo.com>
2023-05-27 15:55:57 -0600

    test: add last placement test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -42,6 +42,10 @@ RSpec.describe Board do
     expect(@board.valid_placement?(@cruiser, ["A1", "B2", "C3"])).to eq(false)
     expect(@board.valid_placement?(@submarine, ["C2", "D3"])).to eq(false)
   end
-  
+
+  it "validates placement" do
+    expect(@board.valid_placement?(@submarine, ["A1", "A2"])).to eq(true)
+    expect(@board.valid_placement?(@cruiser, ["B1", "C1", "D1"])).to eq(true)
+  end
 
 end 
