7fc91a1ef9f883633f3ee4f7d498868028d6b9e4
Gabriel Torres <thomastgtorres21@gmail.com>
2023-05-29 23:26:49 -0500

    test: create game spec

--- lib/game.rb
+++
@@ -1,8 +0,0 @@
-class Game
-
-  def main_menu 
-    puts "Welcome Mariner!! To BATTLESHIP. Ready up.
-    Enter p to play. Enter q to quit."
-  end
-  
-end
---
+++ spec/game_spec.rb
@@ -0,0 +1,17 @@
+require './lib/board'
+require './lib/ship'
+require './lib/cell'
+require './game/cell'
+
+RSpec.describe Game do
+  before(:each) do
+    @game = Game.new
+    @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
+  end
+
+  it 'exist' do 
+      expect(@board).to be_an_instance_of(Board)
+  end
+end 
