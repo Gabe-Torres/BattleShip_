483680e88c711c2bb03cca757cad3272753c22a8
Derek Chavez <frrenose17@yahoo.com>
2023-05-31 20:19:48 -0600

    test: add has ships test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,27 +7,22 @@ RSpec.describe Game do
   before(:each) do
     @game = Game.new
     @board = Board.new
-    @cruiser = Ship.new("Cruiser", 3)
-    @submarine = Ship.new("Submarine", 2)
+    @player_cruiser = Ship.new("Cruiser", 3)
+    @player_submarine = Ship.new("Submarine", 2)
+    @computer_cruiser = Ship.new("Cruiser", 3)
+    @computer_submarine = Ship.new("Submarine", 2)
   end
 
   it 'exist' do 
       expect(@game).to be_an_instance_of(Game)
-  end 
-
-  it "has a main menu" do 
-    expect(@game.main_menu).to eq(puts "string")
   end
 
-  it "sets up game" do 
-
+  it 'has ships' do
+    expect(@player_cruiser).to be_a(Ship)
+    expect(@player_submarine).to be_a(Ship)
+    expect(@computer_cruiser).to be_a(Ship)
+    expect(@computer_submarine).to be_a(Ship)
   end
 
-  it "places player ships" do 
-
-  end
-
-  it "places computer ships" do 
-
-  end 
+ 
 end 
